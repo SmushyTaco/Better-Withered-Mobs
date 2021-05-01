ed545944968b01e4ff79a5fa0a02001d11d84331
SmushyTaco <smushytaco@gmail.com>
2021-04-30 23:42:00 -0700

    Removes Bones Drops From Withered Mobs With A Toggle

---
+++ Better Withered Mobs/src/main/java/com/smushytaco/better_withered_mobs/mixin/LootManagerInject.java
@@ -0,0 +1,25 @@
+package com.smushytaco.better_withered_mobs.mixin;
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.JsonObject;
+import com.smushytaco.better_withered_mobs.mixin_logic.LootManagerInjectLogic;
+import net.minecraft.loot.LootManager;
+import net.minecraft.loot.LootTable;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.profiler.Profiler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.Map;
+@Mixin(LootManager.class)
+public class LootManagerInject {
+    @Shadow
+    private Map<Identifier, LootTable> tables;
+    @Inject(method = "apply", at = @At("RETURN"))
+    private void apply(Map<Identifier, JsonObject> objectMap, ResourceManager manager, Profiler profiler, CallbackInfo info) {
+        LootManager lootManager = (LootManager) (Object) this;
+        tables = ImmutableMap.copyOf(LootManagerInjectLogic.INSTANCE.invokeEarlyLootTableLoadingCallback(tables, manager, lootManager));
+    }
+}
--- Better Withered Mobs/src/main/kotlin/com/smushytaco/better_withered_mobs/BetterWitheredMobs.kt
+++ Better Withered Mobs/src/main/kotlin/com/smushytaco/better_withered_mobs/BetterWitheredMobs.kt
@@ -2,9 +2,13 @@ package com.smushytaco.better_withered_mobs
 import com.smushytaco.better_withered_mobs.bone_meal.WitheredBoneMeal
 import com.smushytaco.better_withered_mobs.bone_meal.WitheredBoneMealDispenserBehavior
 import com.smushytaco.better_withered_mobs.configuration_support.ModConfiguration
+import com.smushytaco.better_withered_mobs.event.SecondaryLootTableLoadingCallback
 import com.smushytaco.better_withered_mobs.fireproof_items.FireproofBlockItem
 import com.smushytaco.better_withered_mobs.fireproof_items.FireproofItem
 import com.smushytaco.better_withered_mobs.fireproof_items.FireproofSyntacticSugar.fireproof
+import com.smushytaco.better_withered_mobs.loot_table_modification.LootTableModificationSyntacticSugar.entries
+import com.smushytaco.better_withered_mobs.loot_table_modification.LootTableModificationSyntacticSugar.item
+import com.smushytaco.better_withered_mobs.loot_table_modification.LootTableModificationSyntacticSugar.pools
 import me.shedaniel.autoconfig.AutoConfig
 import me.shedaniel.autoconfig.annotation.Config
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer
@@ -12,11 +16,11 @@ import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback
 import net.minecraft.block.*
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.loot.LootPool
 import net.minecraft.loot.UniformLootTableRange
 import net.minecraft.loot.entry.ItemEntry
 import net.minecraft.loot.function.LootingEnchantLootFunction
@@ -32,7 +36,7 @@ object BetterWitheredMobs : ModInitializer {
         Items.WITHER_SKELETON_SKULL.fireproof = true
         PotionOfDecay.createPotionRecipes()
 
-        AutoConfig.register(ModConfiguration::class.java) { definition: Config?, configClass: Class<ModConfiguration?>? ->
+        AutoConfig.register(ModConfiguration::class.java) { definition: Config, configClass: Class<ModConfiguration> ->
             GsonConfigSerializer(definition, configClass)
         }
         config = AutoConfig.getConfigHolder(ModConfiguration::class.java).config
@@ -44,11 +48,29 @@ object BetterWitheredMobs : ModInitializer {
         Registry.register(Registry.ITEM, Identifier(MOD_ID, "withered_bone_meal"), WITHERED_BONE_MEAL)
         DispenserBlock.registerBehavior(WITHERED_BONE_MEAL, WitheredBoneMealDispenserBehavior)
         Registry.register(Registry.ENCHANTMENT, Identifier(MOD_ID, "withering"), WITHERING_ENCHANTMENT)
-
-        LootTableLoadingCallback.EVENT.register(LootTableLoadingCallback { _, _, id, supplier, _ ->
+        SecondaryLootTableLoadingCallback.EVENT.register(SecondaryLootTableLoadingCallback { _, lootManager, id, _, setter ->
             if ("minecraft:entities/wither_skeleton" == id.toString() ||
                 "betternether:entities/naga" == id.toString() ||
                 "betternether:entities/skull" == id.toString()) {
+                // Removes Bone Drops
+                if (config.witheredMobsDontDropRegularBones) {
+                    val lootPoolsToRemove = arrayListOf<LootPool>()
+                    lootManager.getTable(id).pools.forEach { lootPool ->
+                        lootPool.entries.forEach { lootPoolEntry ->
+                            if (lootPoolEntry is ItemEntry && Registry.ITEM.getRawId(lootPoolEntry.item) ==
+                                Registry.ITEM.getRawId(Items.BONE)
+                            ) {
+                                lootPoolsToRemove.add(lootPool)
+                            }
+                        }
+                    }
+                    lootPoolsToRemove.forEach { lootPool ->
+                        val lootPoolListClone = lootManager.getTable(id).pools.toMutableList()
+                        lootPoolListClone.removeAll { it == lootPool }
+                        lootManager.getTable(id).pools = lootPoolListClone.toTypedArray()
+                    }
+                }
+                // Adds Withered Bone Drops
                 val lootFunctionOne =
                     SetCountLootFunction.builder(UniformLootTableRange(0.0F, 2.0F))
                 val lootFunctionTwo =
@@ -56,7 +78,11 @@ object BetterWitheredMobs : ModInitializer {
                 val poolBuilder = FabricLootPoolBuilder.builder()
                     .rolls(UniformLootTableRange(0.0F, 2.0F))
                     .with(ItemEntry.builder(WITHERED_BONE).apply(lootFunctionOne)).apply(lootFunctionTwo)
-                supplier.pool(poolBuilder)
+                val lootPoolListClone = lootManager.getTable(id).pools.toMutableList()
+                lootPoolListClone.add(poolBuilder.build())
+                lootManager.getTable(id).pools = lootPoolListClone.toTypedArray()
+                // Applies changes to loot table.
+                setter.set(lootManager.getTable(id))
             }
         })
     }
