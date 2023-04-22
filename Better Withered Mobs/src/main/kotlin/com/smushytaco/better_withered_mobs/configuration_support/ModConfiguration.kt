4b65ebc722bc3d81210b494d74e1b80f74867f45
Nikan Radan <github.com@alias.nikanradan.com>
2023-04-22 00:54:25 -0700

    1.19.4

--- Better Withered Mobs/src/main/kotlin/com/smushytaco/better_withered_mobs/configuration_support/ModConfiguration.kt
+++ Better Withered Mobs/src/main/kotlin/com/smushytaco/better_withered_mobs/configuration_support/ModConfiguration.kt
@@ -2,6 +2,7 @@ package com.smushytaco.better_withered_mobs.configuration_support
 import com.smushytaco.better_withered_mobs.BetterWitheredMobs
 import me.shedaniel.autoconfig.ConfigData
 import me.shedaniel.autoconfig.annotation.Config
+import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.RequiresRestart
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment
 @Config(name = BetterWitheredMobs.MOD_ID)
 class ModConfiguration: ConfigData {
@@ -13,6 +14,7 @@ class ModConfiguration: ConfigData {
     val witheringEnchantmentCanBeAnviledOntoAxes = true
     @Comment("Default value is yes. If set to yes you'll be able to anvil the withering enchantment onto tridents. If set to no you won't be able to.")
     val witheringEnchantmentCanBeAnviledOntoTridents = true
+    @RequiresRestart
     @Comment("Default value is 5. This will determine the maximum level for the withering enchantment. If set to 0 or below the maximum level will be 1. Restart required for changes to take place.")
     val maximumLevelForWitheringEnchantment = 5
     @Comment("Default value is 60. This multiplier will determine how many ticks the withering enchantment will last for. This value will be multiplied with the level to determine the length of the withering effect given.")
